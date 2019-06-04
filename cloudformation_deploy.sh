274d836ede9400566777893cea8662e61bbd5d8c
adi0229 <sideoff@qq.com>
2019-06-04 18:19:37 +0800

    initial

--- cloudformation_deploy.sh
+++ cloudformation_deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-BUCKET_NAME=devopstar
+BUCKET_NAME=adi0229
 
 ## Creates S3 bucket
 aws s3 mb s3://$BUCKET_NAME
 
 ## S3 cloudformation deployments
 ### Base
-aws s3 cp cloudformation/base/fargate-cluster.yaml s3://$BUCKET_NAME/resources/gpt-2-flask-api/cloudformation/base/fargate-cluster.yaml
+aws s3 cp cloudformation/base/fargate-cluster.yaml s3://$BUCKET_NAME/resources/gpt-2-flask/cloudformation/base/fargate-cluster.yaml
 aws s3 cp cloudformation/base/fargate-service.yaml s3://$BUCKET_NAME/resources/gpt-2-flask-api/cloudformation/base/fargate-service.yaml
-aws s3 cp cloudformation/base/vpc-networking.yaml s3://$BUCKET_NAME/resources/gpt-2-flask-api/cloudformation/base/vpc-networking.yaml
+aws s3 cp cloudformation/base/vpc-networking.yaml s3://$BUCKET_NAME/resources/gpt-2-flask/cloudformation/base/vpc-networking.yaml
 ### CI/CD
-aws s3 cp cloudformation/cicd/codebuild.yaml s3://$BUCKET_NAME/resources/gpt-2-flask-api/cloudformation/cicd/codebuild.yaml
+aws s3 cp cloudformation/cicd/codebuild.yaml s3://$BUCKET_NAME/resources/gpt-2-flask/cloudformation/cicd/codebuild.yaml
--- container_push.sh
+++ container_push.sh
@@ -1,9 +1,9 @@
 # Create ECR (if not already existing)
-aws ecr create-repository --repository-name "gpt-2-flask-api"
+aws ecr create-repository --repository-name "gpt-2-flask"
 
-ACCOUNT_ID=$(aws sts get-caller-identity |  jq -r '.Account')
+ACCOUNT_ID=780285191874
 $(aws ecr get-login --no-include-email --region us-east-1)
 
-docker build -t gpt-2-flask-api .
-docker tag gpt-2-flask-api:latest $ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/gpt-2-flask-api:latest
-docker push $ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/gpt-2-flask-api:latest
+docker build -t gpt-2-flask .
+docker tag gpt-2-flask-api:latest 780285191874.dkr.ecr.us-east-1.amazonaws.com/gpt-2-flask:latest
+docker push 780285191874.dkr.ecr.us-east-1.amazonaws.com/gpt-2-flask:latest
